a69fd761f8004802165f9ab4accf14d28c049acb
klymkov_ivan <klymkov90@gmail.com>
2025-07-15 15:35:19 +0300

    done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,51 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(ms => {
+      console.log(`✅ Fulfilled promise in ${ms}ms`);
+      iziToast.show({
+        iconUrl: '../img/success.svg',
+        title: 'OK',
+        message: `Fulfilled promise in ${delay} ms`,
+        position: 'topCenter',
+        color: '#59a10d',
+        messageColor: '#fff',
+        titleColor: '#fff',
+      });
+    })
+    .catch(ms => {
+      console.log(`❌ Rejected promise in ${ms}ms`);
+      iziToast.show({
+        iconUrl: '../img/error.svg',
+        title: 'Error',
+        message: 'Illegal operation',
+        position: 'topCenter',
+        color: '#ef4040',
+        messageColor: '#fff',
+        titleColor: '#fff',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
