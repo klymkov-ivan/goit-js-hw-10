2dca42c10bbf38bd747a11a985124d6b08c71b11
klymkov_ivan <klymkov90@gmail.com>
2025-07-15 15:55:06 +0300

    public path

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -13,7 +13,7 @@ form.addEventListener('submit', event => {
     .then(ms => {
       console.log(`✅ Fulfilled promise in ${ms}ms`);
       iziToast.show({
-        iconUrl: '../public/success.svg',
+        iconUrl: '/success.svg',
         title: 'OK',
         message: `Fulfilled promise in ${delay} ms`,
         position: 'topCenter',
@@ -25,7 +25,7 @@ form.addEventListener('submit', event => {
     .catch(ms => {
       console.log(`❌ Rejected promise in ${ms}ms`);
       iziToast.show({
-        iconUrl: '../public/error.svg',
+        iconUrl: '/error.svg',
         title: 'Error',
         message: 'Illegal operation',
         position: 'topCenter',
